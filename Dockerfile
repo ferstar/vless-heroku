25a5633d7cc1c70465548841abe647125690a5a5
J. F. Zhang <zhangjianfei3@gmail.com>
2022-04-11 04:11:11 +0800

    Optimize docker pipe

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,42 @@
-FROM v2fly/v2fly-core:latest
+FROM ubuntu:20.04
 
-RUN wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 -O /root/cloudflared
-RUN chmod +x /root/cloudflared
-ADD startup.sh /startup.sh
-RUN chmod +x /startup.sh
+ENV LANG=en_US.UTF-8 \
+    TZ=Asia/Shanghai \
+    SUPERVISOR_LOGFILE_BACKUPS=3 \
+    DEBIAN_FRONTEND=noninteractive
 
-CMD /startup.sh
+COPY v2ray.sh /root/v2ray.sh
+COPY startup.sh /startup.sh
 
+RUN apt update && \
+    apt install -y --no-install-recommends \
+    python3.8-dev \
+    python3.8-distutils \
+    openssl \
+    ca-certificates \
+    wget \
+    curl \
+    tzdata \
+    locales \
+    python3-pip \
+    vim-tiny \
+    htop && \
+    ln -sf /usr/bin/python3.8 /usr/bin/python3 && \
+    ln -sf /usr/bin/vim-tiny /usr/bin/vim && \
+    ln -sf /usr/bin/python3.8-config /usr/bin/python-config && \
+    locale-gen en_US.UTF-8 && \
+    pip3 install --upgrade --no-cache-dir pip && \
+    pip3 install --upgrade --no-cache-dir setuptools wheel && \
+    pip3 install --upgrade --no-cache-dir supervisor && \
+    mkdir -pv /etc/supervisor/conf.d && \
+    rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* /root/.cache && \
+    mkdir -p /etc/v2ray /usr/local/share/v2ray /var/log/v2ray && \
+    chmod +x /root/v2ray.sh && \
+    chmod +x /startup.sh && \
+    /root/v2ray.sh && \
+    wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 -O /root/cloudflared && \
+    chmod +x /root/cloudflared
 
+ENTRYPOINT ["/startup.sh"]
+
+HEALTHCHECK --interval=30s --timeout=30s --start-period=5s --retries=3 CMD /usr/bin/nc -z localhost 8080 || exit 1
--- startup.sh
+++ startup.sh
@@ -1,4 +1,38 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e
+
+rm -rf /etc/supervisor/conf.d/*
+
+cat << EOF > /etc/supervisor/supervisord.conf
+[unix_http_server]
+file=/dev/shm/supervisor.sock   ; (the path to the socket file)
+chmod=0700                      ; sockef file mode (default 0700)
+
+[supervisord]
+logfile=/root/supervisord.log ; (main log file;default $CWD/supervisord.log)
+pidfile=/dev/shm/supervisord.pid ; (supervisord pidfile;default supervisord.pid)
+childlogdir=/root            ; ('AUTO' child log dir, default $TEMP)
+
+[rpcinterface:supervisor]
+supervisor.rpcinterface_factory = supervisor.rpcinterface:make_main_rpcinterface
+
+[supervisorctl]
+serverurl=unix:///dev/shm/supervisor.sock ; use a unix:// URL  for a unix socket
+
+[include]
+files = /etc/supervisor/conf.d/*.conf
+EOF
+
+cat << EOF > /etc/supervisor/conf.d/v2ray.conf
+[program:v2ray]
+command=/usr/bin/v2ray -config /etc/v2ray/config.json
+autorestart=true
+autostart=true
+startsecs=10
+stdout_logfile=/dev/stdout
+stderr_logfile=/dev/stderr
+EOF
 
 cat << EOF > /tmp/config.json
 {
@@ -48,11 +82,17 @@ cat << EOF > /tmp/config.json
 }
 EOF
 
-# Run V2Ray
 if [[ $TUNNEL_TOKEN ]]; then
-echo 'has tunnel token, run cloudflared tunnel'
-/usr/bin/nohup /usr/bin/v2ray -config /tmp/config.json > /dev/null 2>&1 & \
-    /root/cloudflared tunnel --no-autoupdate run --token $TUNNEL_TOKEN
-else
-/usr/bin/v2ray -config /tmp/config.json
+    echo 'has tunnel token, run cloudflared tunnel'
+    cat << EOF >> /etc/supervisor/conf.d/v2ray.conf
+[program:cloudflared]
+command=/root/cloudflared tunnel --no-autoupdate run --token $ENV_TUNNEL_TOKEN
+autorestart=true
+autostart=true
+startsecs=10
+stdout_logfile=/dev/stdout
+stderr_logfile=/dev/stderr
+EOF
 fi
+
+supervisord -n -c /etc/supervisor/supervisord.conf
